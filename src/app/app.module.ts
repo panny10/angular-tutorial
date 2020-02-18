7a8a4f7dfd4316787554bb5e6913a65e80816c7d
panny10 <michalpanuska@seznam.cz>
2020-02-18 14:14:59 +0100

    2 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
 import { CoursesComponent } from "./courses.component";
+import { CoursesService } from "./courses.service";
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
   declarations: [AppComponent, HelloComponent, CoursesComponent ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  providers: [CoursesService]
 })
 export class AppModule {}
--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,18 +1,16 @@
-import { Component } from '@angular/core'
+import { CoursesService } from './courses.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'courses',
-  template: `
-    <h2>{{ title }}</h2>
-    <ul>
-      <li *ngFor="let course of courses">
-        {{ course }}
-      </li>
-    </ul>
-  `,
+  templateUrl: './courses.component.html',
   styleUrls: [ './courses.component.css' ]
 })
 export class CoursesComponent {
   title = "List of courses";
-  courses = ["course1", "course2", "course3"];
+  courses;
+
+  constructor(service: CoursesService) {
+    this.courses = service.getCourses();
+  }
 }
